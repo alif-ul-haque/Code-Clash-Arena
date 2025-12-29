ebf2dcb7d2ea0783d3181ab19b1ce16ade8c6fe5
Alif Ul Haque <alifulhaque19@gmail.com>
2025-12-30 02:20:03 +0600

    added the search block and magnifying glass search part

--- src/pvp_battle/pages/1v1_global_page.jsx
+++ src/pvp_battle/pages/1v1_global_page.jsx
@@ -1,13 +1,22 @@
 import React from 'react';
 import '../style/1v1_global_page.css';
+import magnifyingGlassIcon from '../../assets/icons/magnifying_glass.png';
+import swordsIcon from '../../assets/icons/swords.png';
 
 const OneVOneGlobalPage = () => {
     return (
         <div className="one-v-one-global-page">
             <div className="page-container"></div>  {/* Blurred background */}
             <div className="content-wrapper">
-                {/* Your sharp content goes here */}
-                <h1>SEARCHING FOR A OPPONENT</h1>
+                <div className="content-wrapper">
+                    <div className="search-card">
+                        <h1 className="search-heading">SEARCHING FOR A OPPONENT</h1>
+                        <p className="animated-dots">••••••••••••••••••••••••</p>
+                        <div className="search-icon">
+                            <img src={magnifyingGlassIcon} alt="search" />
+                        </div>
+                    </div>
+                </div>
             </div>
         </div>
     );
