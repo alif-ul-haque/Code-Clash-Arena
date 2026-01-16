fa83dc854eb0fb1669bae24d4823e748783e283c
Alif Ul Haque <alifulhaque19@gmail.com>
2026-01-17 01:34:40 +0600

    Added 1v1 battle tables and initial user data inserts; updated CSS for friends list styling

--- src/Code_Clash_Arena.sql
+++ src/Code_Clash_Arena.sql
@@ -77,3 +77,47 @@ values (
 );
 
 select current_database();
+
+CREATE table onevonebattles (
+  onevone_battle_id UUID PRIMARY KEY default gen_random_uuid(),
+  battlefield VARCHAR(100),
+  battle_mode VARCHAR(100),
+  problem_count INTEGER,
+  status VARCHAR(20) NOT NULL,
+  trophy_reward INTEGER, 
+  start_time TIMESTAMP,
+  end_time TIMESTAMP
+);
+
+create table onevone_participants (
+  onevone_battle_id UUID REFERENCES onevonebattles(onevone_battle_id) ON DELETE CASCADE,
+  player_id UUID REFERENCES users(id) ON DELETE CASCADE,
+  problem_solved INTEGER DEFAULT 0,
+  time_taken INTEGER DEFAULT 0,
+  PRIMARY KEY (onevone_battle_id, player_id)
+);
+
+
+insert into users (email, cf_handle, xp, trophy, clan_id)
+values ('alifulhaque19@gmail.com', 'alif_uL_haQue', 50.25, 1500, null);
+
+insert into users (email, cf_handle, xp, trophy, clan_id)
+values ('ibnatmatin.com', 'Matin008', 70.25, 1900, null);
+
+insert into users (email, cf_handle, xp, trophy, clan_id)
+values ('takli@gmail.com', 'takli096', 66.25, 1650, null);
+
+insert into users (email, cf_handle, xp, trophy, clan_id)
+values ('than@gmail.com', 'than_007', 63.75, 1350, null);
+
+insert into users (email, cf_handle, xp, trophy, clan_id)
+values ('sambani@gmail.com', 'sambani129', 69.50, 1700, null);
+
+
+insert into friends (u_id, f_id, status)
+select 
+  (select id from users where cf_handle = 'alif19') as u_id,
+  id as f_id,
+  'accepted' as status
+from users
+where cf_handle <> 'alif19';
